5b4d735c0e1df1abcdd32d9bb1172cda738da680
L-Edgar <lelishaedgar10@gmail.com>
2024-10-16 08:43:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Button from "./components/button/Button";
+import Hero from "./components/hero/Hero";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Header/>
+        <Hero/>
+        <Footer/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
